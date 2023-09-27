e000eec10a2cc7fce1e842695f02175aaa106fb8
Karimah Reavis <Karimah586@gmail.com>
2023-09-27 03:49:20 -0400

    Always write a commit message to explain what you changed in your project files

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    
+    return(
+
+        <div className="header-wrapper">
+            <h1>Hello World</h1>
+            <p>I am making changes to my repository.</p>
+
+        </div>
+    );
+}
+
+export default Header;
